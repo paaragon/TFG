5a045e07c3e0c545907dec00bfa0dbad8b5cc3f7
MrSlide22 <pablo.aragon22@gmail.com>
2016-10-21 12:51:56 +0200

    Fix csv creation and add order by date

--- fetchingDataExample.py
+++ fetchingDataExample.py
@@ -5,19 +5,19 @@ Created on Wed Oct 19 14:09:47 2016
 @author: slide22
 """
 
-from infoRiegoData import dataManager as dM
+#from infoRiegoData import dataManager as dM
 from infoRiegoData import csvManager as cM
 
-dM.downloadData(2001, 2002, 'data/zipFiles/')
-dM.uncompressData('data/zipFiles/', 'data/csvFiles/')
-dM.correctCharacters('data/csvFiles/')
+#dM.downloadData(2001, 2002, 'data/zipFiles/')
+#dM.uncompressData('data/zipFiles/', 'data/csvFiles/')
+#dM.correctCharacters('data/csvFiles/')
 
 conditions = dict()
 conditions['dateStart'] = 20010101
-conditions['dateEnd'] = 20010102
-conditions['hourStart'] = 100
-conditions['hourEnd'] = 200
-conditions['ubication'] = ['Nava de Arevalo', 'Miranda de Ebro']
+conditions['dateEnd'] = 20020101
+#conditions['hourStart'] = 100
+#conditions['hourEnd'] = 200
+conditions['ubication'] = ['Nava de Arevalo']
 
 cM.createCSVWithConditions('data/csvFiles/', 'data/filteredFile.csv', conditions)
 
--- infoRiegoData/csvManager.py
+++ infoRiegoData/csvManager.py
@@ -19,7 +19,7 @@ Args:
                  - endHour (int): the hout on which ends csv. Format: HHMM
                  - ubication (list): list with the ubications of the csv
 """
-def createCSVWithConditions(sourceFolder, destinationPath, cond = dict()):
+def createCSVWithConditions(sourceFolder, destinationPath, cond = dict(), verbose = True):
     
     import pandas
     from os import listdir
@@ -48,15 +48,24 @@ def createCSVWithConditions(sourceFolder, destinationPath, cond = dict()):
     
     # loading the files
     
+    i = 0
     for f in csvFiles:
+        
+        i += 1
+        if verbose:
+            print 'Reading '+ f + '('+ str(i) +'/'+ str(len(csvFiles)) +')'
+            
         if 'df' not in locals():
             df = pandas.read_csv(sourceFolder + f, ';')
         else:
-            df.append(pandas.read_csv(sourceFolder + f, ';'))
+            df = df.append(pandas.read_csv(sourceFolder + f, ';'))
     
     df = pandas.DataFrame(df[(df['Hora (HHMM)'] >= conditions['hourStart']) \
                            & (df['Hora (HHMM)'] <= conditions['hourEnd']) \
                            & (df['Ubicacion'].isin(conditions['ubication']))])
+    
+    df = df.sort_values(by=['Fecha (AAAA-MM-DD)', 'Hora (HHMM)'], ascending=True)
+                           
                            
     df.to_csv(destinationPath, index = False)
     
@@ -64,9 +73,9 @@ if __name__ == "__main__":
 
     conditions = dict()
     conditions['dateStart'] = 20010101
-    conditions['dateEnd'] = 20010101
-    conditions['hourStart'] = 100
-    conditions['hourEnd'] = 200
-    conditions['ubication'] = ['Nava de Arevalo', 'Miranda de Ebro']
+    conditions['dateEnd'] = 20020101
+    #conditions['hourStart'] = 100
+    #conditions['hourEnd'] = 200
+    conditions['ubication'] = ['Nava de Arevalo']
     
-    createCSVWithConditions('../csvFiles/', 'file.csv', conditions)
+    createCSVWithConditions('../data/csvFiles/', '../data/filteredFile.csv', conditions)
