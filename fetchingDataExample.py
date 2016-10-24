79aa5c2ac4e42698cdf6fe5ab2d29aca4713a748
abelCoronado93 <acoronad@ucm.es>
2016-10-24 17:42:24 +0200

    New python file with a linear regression example

--- fetchingDataExample.py
+++ fetchingDataExample.py
@@ -5,19 +5,22 @@ Created on Wed Oct 19 14:09:47 2016
 @author: slide22
 """
 
-#from infoRiegoData import dataManager as dM
+from infoRiegoData import dataManager as dM
 from infoRiegoData import csvManager as cM
+import pandas
 
-#dM.downloadData(2001, 2002, 'data/zipFiles/')
-#dM.uncompressData('data/zipFiles/', 'data/csvFiles/')
-#dM.correctCharacters('data/csvFiles/')
+dM.downloadData(2001, 2002, 'data/zipFiles/')
+dM.uncompressData('data/zipFiles/', 'data/csvFiles/')
+dM.correctCharacters('data/csvFiles/')
 
 conditions = dict()
 conditions['dateStart'] = 20010101
 conditions['dateEnd'] = 20020101
-#conditions['hourStart'] = 100
-#conditions['hourEnd'] = 200
+conditions['hourStart'] = 100
+conditions['hourEnd'] = 200
 conditions['ubication'] = ['Nava de Arevalo']
 
 cM.createCSVWithConditions('data/csvFiles/', 'data/filteredFile.csv', conditions)
 
+df = pandas.read_csv('data/filteredFile.csv')
+print df
