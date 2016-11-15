cc1ce8e3091858a4e8b025c3ca19440923ea42fd
MrSlide22 <pablo.aragon22@gmail.com>
2016-11-15 12:12:28 +0100

    fixed merge problems

--- predictExample.py
+++ predictExample.py
@@ -43,14 +43,12 @@ conditions['ubication'] = ['Nava de Arevalo']
 cM.createCSVWithConditions('data/csvFiles/',\
                            'data/filteredFile.csv',\
                            conditions)
->>>>>>> b40cf697f14f61f4bbed84f582dd6e185e21854a
 
 conditionsX = dict()
 conditionsX['relativeTargetSample'] = 2
 conditionsX['nSamples'] = 4
 #x.generateDataFrame('data/filteredFile.csv','data/x.csv', conditionsX)
 
-<<<<<<< HEAD
 conditionsY = dict()
 #y.generateDataFrame('data/filteredFile.csv', 'data/x.csv', 'data/y.csv')
 
@@ -103,51 +101,10 @@ newDf = pd.DataFrame(data = data, columns = columns)
 resultDf = resultDf.append(newDf)
 
 resultDf.to_csv('data/results.csv', index = False)
-=======
-#cleaning DataFrame
-df = df.drop('Ubicacion', 1)
-
-newEntry = pandas.Series(['AV01', '2001-06-06', 1900, None, None, None, None, None, None])
-
-newDf = pandas.DataFrame([list(newEntry)],\
-                         columns=['Codigo', 'Fecha (AAAA-MM-DD)',\
-                                 'Hora (HHMM)', 'Humedad relativa (%)', 'Precipitacion (mm)',\
-                                 'Radiacion (W/m2)', 'Temperatura (oC)', 'Vel. viento (m/s)', 'Dir. viento (o)'])
-                         
-df = df.append(newDf, ignore_index=True)
-
-df['Fecha (AAAA-MM-DD)'] = [item.replace('-', '') for item in df['Fecha (AAAA-MM-DD)']]
-   
-for i, codigo in enumerate(df['Codigo'].unique()):
-    df.loc[df["Codigo"]==codigo, "Codigo"] = i
-
-#df['Radiacion (W/m2)'] = np.array([item*100 for item in df['Radiacion (W/m2)']]).astype(long)
-
-predictors = ['Codigo', 'Hora (HHMM)', 'Fecha (AAAA-MM-DD)']
-targetIndexes = ["Precipitacion (mm)", "Temperatura (oC)",\
-                 "Humedad relativa (%)", "Radiacion (W/m2)",\
-                 "Vel. viento (m/s)", "Dir. viento (o)"]
-
-#Defining data and target
-solar_data = df
-solar_target = df[targetIndexes]
-                  
-#print solar_data
-#print solar_target
-
-#X_train, X_test, y_train, y_test = train_test_split(solar_data, solar_target,\
-#                                                    test_size=0.4,\
-#                                                    random_state=0)
+                                                 random_state=0)
 
 alg = linear_model.LinearRegression()
 
 #scores = cross_val_score(alg, solar_data[:-1], solar_target[:-1], cv=5)
 #print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-kf = KFold(n_splits=2)
-for train, test in kf.split(solar_data[:-1]):
-    alg.fit(solar_data[predictors].loc[train], solar_target.loc[train])
-    
-    
-print alg.predict(solar_data[predictors].iloc[-1:])
->>>>>>> b40cf697f14f61f4bbed84f582dd6e185e21854a
