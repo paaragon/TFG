c483668b6815b9e9f7264569d87be3b1a888821c
MrSlide22 <pablo.aragon22@gmail.com>
2016-11-29 22:09:18 +0100

    mqtt client and thingSpeak bridge

--- mqttClient.py
+++ mqttClient.py
@@ -8,6 +8,15 @@ Created on Mon Nov 21 16:25:16 2016
 import paho.mqtt.client as mqtt
 import json
 
+def listen_on_connect(client, userdata, rc):
+    print "Connected with result code "+str(rc)
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("solar")
+
+def listen_on_message(client, userdata, msg):
+    print msg.topic+" "+str(msg.payload)
+
 def sendToBroker(brokerIp, brokerPort, payload, topic):
     
     client = mqtt.Client()
@@ -19,9 +28,20 @@ def sendToBroker(brokerIp, brokerPort, payload, topic):
     print "Succesfully published"
 
 def listenToBroker(brokerIp, brokerPort, topic):
-    
+
+    client = mqtt.Client()
+    client.on_connect = listen_on_connect
+    client.on_message = listen_on_message
+
+    client.connect("127.0.0.1", 1883, 60)
+
+    client.loop_forever()
 
 if __name__ == "__main__":
+    '''
     data = {"codigo": 1,\
             "ubicación": "Navas de Arévalo"}
     sendToBroker("127.0.0.1", 1883, json.dumps(data), "solar")
+    '''
+
+    listenToBroker("127.0.0.1", 1883, "solar")
---
+++ thingSpeakBridge.py
@@ -0,0 +1,35 @@
+import httplib, urllib
+import time
+
+
+def sendToThingSpeak(data):
+
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded",\
+               "Accept": "text/plain"}
+    params = urllib.urlencode(data)
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    
+    try:
+        
+        conn.request("POST", "/update", params, headers)
+        response = conn.getresponse()
+        
+        print response.status, response.reason
+        
+        data = response.read()
+        
+        print data
+        
+        conn.close()
+        
+    except:
+        print "connection failed"
+
+if __name__ == "__main__":
+
+    from random import randint
+    import pandas as pd
+    for i in range(0, 30):
+        data = {'field4': randint(0, 500), 'key': 'O2M7W8NYQL5X7XD3'}
+        sendToThingSpeak(data)
+        time.sleep(16)
