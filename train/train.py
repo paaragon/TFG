12319c55427f2db9f6345d6750ef8a30a537f288
MrSlide22 <pablo.aragon22@gmail.com>
2017-03-23 18:14:45 +0100

    Save results in file

--- train/train.py
+++ train/train.py
@@ -43,8 +43,7 @@ for k in kList:
     print ""
     means = clf.cv_results_['mean_test_score']
     stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print "%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params)
-
     with open(tuned_parameters['id']+'.csv', 'w') as csvfile:
-	write("%0.3f,%0.03f,%r" % (mean, std * 2, params))
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print "%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params)
+        write("%0.3f,%0.03f,%r" % (mean, std * 2, params))
