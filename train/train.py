410361081e3a4a1e0eb1ade887d06afad3b72e71
MrSlide22 <pablo.aragon22@gmail.com>
2017-05-26 16:28:39 +0200

    Regression train fixed

--- train/train.py
+++ train/train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 
 
-with open('./conf/mlpclassifier.json') as data_file:
+with open('./conf/regression.json') as data_file:
     conf = json.load(data_file)
 
 kList = conf['k']
@@ -30,8 +30,8 @@ for k in kList:
         X = solarData.getData()
         y = solarData.getTarget()
 
-        print X.shape
-        print y.shape
+        print "X shape: ", X.shape
+        print "Y shape: ", y.shape
 
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.3, random_state=0)
@@ -49,10 +49,10 @@ for k in kList:
         means = clf.cv_results_['mean_test_score']
         stds = clf.cv_results_['std_test_score']
 
-        with open('results/' + conf['id'] + str(k) + '.csv', 'w') as csvfile:
+        with open('results/' + conf['id'] + '.csv', 'a') as csvfile:
             for mean, std, params in zip(means, stds, clf.cv_results_['params']):
                 print "%f (+/-%f) for %r" % (mean, std * 2, params)
-                csvfile.write("%i;%f;%f;%r;%s;%s\n" % (
-                    k, mean, std * 2, params, clf.best_params_, str(model)))
+                csvfile.write("%s;%i;%f;%f;%r;%s\n" % (
+                    estimator['model'], k, mean, std * 2, params, clf.best_params_))
 
 print "Test finished"
